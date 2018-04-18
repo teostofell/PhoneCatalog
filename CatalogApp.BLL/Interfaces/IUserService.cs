3fa33a5dca21bd9fb43c16ec1e4002f15fca2839
teostofell <teostofell@gmail.com>
2018-04-18 18:04:20 +0300

    Fixes

--- CatalogApp.BLL/Interfaces/IUserService.cs
+++ CatalogApp.BLL/Interfaces/IUserService.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace CatalogApp.BLL.Interfaces
 {
-    public interface IUserService
+    // TODO: I implemented IDisposeable here cuz I need to use it in using()-blocks (see Provider class in API layer)
+    public interface IUserService : IDisposable
     {
         Task<OperationDetails> Register(UserDTO user);
         Task<UserDTO> FindUser(string userName, string password);
